aa7f0e19935e42e260bc1ff94be2b0b8f2e2d3b7
Cari & Lukas Jones <pair+cariwest.lukasjones@devbootcamp.com>
2015-02-27 11:57:11 -0800

    errors display if username and/or password is wrong

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,4 +1,5 @@
 get '/' do
+  @errors = []
   @user = User.new
   erb :index
 end
@@ -6,15 +7,33 @@ end
 post '/user/login' do
   user = User.where(username: params[:username]).first
 
-  # raise error if user login fails...
-  if user != "nil"
-    if user.authenticate(params[:password])
-      session[:id] = user.id
-      redirect "/user"
-    else
-      redirect '/'
-    end
+  @errors = []
+
+  if user == nil
+    @errors << "User does not exist"
+  elsif user.authenticate(params[:password]) == false
+    @errors << "Password does not match"
   end
+
+  if @errors.empty?
+    session[:id] = user.id
+    redirect "/user"
+  else
+    @user = User.new
+    erb :index
+  end
+
+
+
+  # raise error if user login fails...
+  # if user != "nil"
+  #   if user.authenticate(params[:password])
+  #     session[:id] = user.id
+  #     redirect "/user"
+  #   else
+  #     redirect '/'
+  #   end
+  # end
 end
 
 post '/user/register' do
