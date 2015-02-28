8b717768b574715d3f1f0d5ca29a600bf1e9527e
Jeremy Gagon & Jessica Grebenschikov <pair+jergagon1.jessygreben@devbootcamp.com>
2015-02-27 16:31:26 -0800

    Add search bar

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,7 +4,12 @@ get '/user/:username' do
   @current_user = User.where(username: params[:username]).first
   @tweets = @current_user.tweets
   @quote = @current_user.quote
-  # p @current_user.tweets.first.content
   erb :'users/profile'
+end
+
+post '/find_user' do
+  user = User.where(username: params[:user][:username]).first
+
+  redirect "/user/#{user.username}"
 end
 
